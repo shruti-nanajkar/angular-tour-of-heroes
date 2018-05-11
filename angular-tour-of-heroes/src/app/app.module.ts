8790b9c851cb5d23821e9122255e4e973b0d1fd8
Shruti Nanajkar <nanajkar.s@husky.neu.edu>
2018-05-11 01:37:18 -0400

    added heroes Component and added two-way data binding for the hero name

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; //ngModule lives here
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //external modules that ar eimported should go here i.e FormsModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
